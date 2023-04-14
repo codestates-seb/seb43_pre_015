0600ce29feecab02b1cb3ba153b963efefaa21a4
chipmunk-dev <devquokkajeong@gmail.com>
2023-04-14 16:38:02 +0900

    feat: #2 - answer entity 작성
    Spring Data JPA를 이용하여 Answer Entity를 작성하였습니다.

---
+++ server/src/main/java/com/pre015/server/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.pre015.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
