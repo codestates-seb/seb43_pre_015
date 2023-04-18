3b1571318b40ec8f07e91868c81138a0385ba3dd
ken3432 <119277912+ken3432@users.noreply.github.com>
2023-04-18 14:52:17 +0900

    member domain (#51)
    * git checkout dev
    
    * answer entity 작성 (#3)
    
    * Create .gitignore (#1)
    
    * feat: #2 - answer entity 작성
    
    Spring Data JPA를 이용하여 Answer Entity를 작성하였습니다.
    
    * Feat: Question Entity 추가 (#5)
    
    * Create .gitignore (#1)
    
    * Feat: Question Entity 추가
    
    ---------
    
    Co-authored-by: chipmunk <107650362+chipmunk-dev@users.noreply.github.com>
    
    * comment entity 작업 (#12)
    
    * Comment Entity 작업
    
    * Comment Entity 작업
    
    * member (#14)
    
    * Delete .idea directory
    
    * feat: 브랜치 통합 (#30)
    
    - dev 브랜치로 작업 통합
    
    * client_dev
    
    * Revert "Revert "Merge branch 'dev'""
    
    This reverts commit 375356a4129e69d9adabfbddef3550650c27db83.
    
    * Revert "Merge branch 'FE_dev'"
    
    This reverts commit a3e981bba6a84e3df83a4af8f01c97fcee08d9c2, reversing
    changes made to db36f4befa3175cbac1bb4c35d59466fd3e2adab.
    
    * revert
    
    * Revert "revert"
    
    This reverts commit 8e43378a0d2bfe78d92619737f3d3d38f9d349a5.
    
    * Revert "Revert "revert""
    
    This reverts commit 37ce2a3f8bf7c431cb6f66fb5540da94b76ea6d7.
    
    * Revert "Revert "Merge branch 'FE_dev'""
    
    This reverts commit c19e7d5acfe11f57fd9b2e71d5eee67b9963609c.
    
    * Revert "Merge branch 'FE_dev'"
    
    This reverts commit a3e981bba6a84e3df83a4af8f01c97fcee08d9c2, reversing
    changes made to db36f4befa3175cbac1bb4c35d59466fd3e2adab.
    
    * Revert "Revert "Revert "Merge branch 'dev'"""
    
    This reverts commit db36f4befa3175cbac1bb4c35d59466fd3e2adab.
    
    * Initial settings
    
    * 1
    
    ---------
    
    Co-authored-by: chipmunk <107650362+chipmunk-dev@users.noreply.github.com>
    Co-authored-by: happy-camel <120029069+happy-camel@users.noreply.github.com>
    Co-authored-by: lunius94 <119986615+lunius94@users.noreply.github.com>
    Co-authored-by: uyV-git <anhw369@naver.com>
    Co-authored-by: uyV-git <85952797+uyV-git@users.noreply.github.com>
    Co-authored-by: Hyojin Ju <hyojin.ju@gmx.net>
    Co-authored-by: Hyojin Ju <110552619+HJNJu@users.noreply.github.com>

---
+++ server/src/main/java/com/pre015/server/member/entity/Member.java
@@ -0,0 +1,80 @@
+package com.pre015.server.member.entity;
+
+import com.pre015.server.answer.entity.Answer;
+import com.pre015.server.comment.entity.Comment;
+import com.pre015.server.question.entity.Question;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "members")
+@Builder
+
+
+public class Member {
+
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false, unique = true)
+    private String displayName;
+
+    @Column(nullable = false, updatable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private LocalDate createdDate;
+
+    @Column(name = "last_modified_at")
+    @LastModifiedDate
+    private LocalDate last_modified_at;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MemberStatus memberStatus = MemberStatus.ACTIVE;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<Qustions> questions = new ArrayList<>();
+    @OneToMany(mappedBy = "member")
+    private List<Answer> answers = new ArrayList<>();
+    @OneToMany(mappedBy = "member")
+    private List<Comment> comments = new ArrayList<>();
+
+    public List<Question> getQuestion() {
+        return questions;
+    }
+
+    public List<Answer> getAnswer() {
+        return answers;
+    }
+
+    public List<Comment> getComment() {
+        return comments;
+    }
+}
+
+
+
+
+
+
+
---
+++ server/src/main/java/com/pre015/server/member/service/MemberService.java
@@ -0,0 +1,55 @@
+package com.pre015.server.member.service;
+
+import com.pre015.server.member.entity.Member;
+import com.pre015.server.member.entity.MemberStatus;
+import com.pre015.server.member.exception.BusinessLogicException;
+import com.pre015.server.member.exception.ExceptionCode;
+import com.pre015.server.member.repository.MemberRepositoty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+
+public class MemberService {
+    private final MemberRepositoty memberRepositoty;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public Member saveMember(Member member){
+        verifyExistEmail(member.getEmail());
+
+        String encryptedPassword = passwordEncoder.encode(member.getPassword());
+        member.setPassword(encryptedPassword);
+
+
+
+        return memberRepositoty.save(member);
+    }
+
+
+
+    private void verifyExistEmail(String email){
+        Optional<Member> member = memberRepositoty.findByEmail(email);
+        if(member.isPresent())
+            throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
+    }
+
+    @Transactional(readOnly = true)
+    public Member findVerifiedMember(Long memberId) {
+        Optional<Member> optionalUser = memberRepositoty.findById(memberId);
+        Member findUser = optionalUser.orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        if (findUser.getMemberStatus() == MemberStatus.RESIGNED) {
+            throw new BusinessLogicException(ExceptionCode.MEMBER_RESIGNED);
+        }
+        return findUser;
+    }
+}
+
+
+
