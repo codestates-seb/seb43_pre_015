396c139f8c6ea8c0557944bf049c6fceaa6dcb85
Hyojin Ju <hyojin.ju@gmx.net>
2023-04-18 19:17:53 +0900

    style: Organize code style

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
+import Activity from './pages/Activity';
+
 
 function App() {
   return (
     <div>
+      <main>
       <Sidebar />
+      </main>
     </div>
   );
 }
