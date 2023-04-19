7c71a5f82843c940c28711e5e7e89ec3ea7147e4
lunius94 <119986615+lunius94@users.noreply.github.com>
2023-04-19 13:21:59 +0900

    에러 부분 수정 및 Security관련 메소드 일시 삭제 (#68)
    Spring rest dock 관계 build.gradle에 추가.

--- server/src/main/java/com/pre015/server/answer/repository/AnswerRepository.java
+++ server/src/main/java/com/pre015/server/answer/repository/AnswerRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
-//    List<Answer> findByMemberId(Long memberId);
-//    List<Answer> findByQuestionId(Long questionId);
+    List<Answer> findByMember(Long memberId);
+    List<Answer> findByQuestion(Long questionId);
 }
