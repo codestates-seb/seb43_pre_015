04253275d5bf5c9bdcc4f667d5c459f2a947067d
chipmunk-dev <devquokkajeong@gmail.com>
2023-04-19 18:33:02 +0900

    #25 - [Test Controller] success

--- server/src/test/java/com/pre015/server/answer/contoller/AnswerControllerTest.java
+++ server/src/test/java/com/pre015/server/answer/contoller/AnswerControllerTest.java
@@ -1,10 +1,12 @@
 package com.pre015.server.answer.contoller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.pre015.server.answer.dto.AnswerDTO;
 import com.pre015.server.answer.entity.Answer;
 import com.pre015.server.answer.service.AnswerService;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +15,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.hamcrest.Matchers.startsWith;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Transactional
 @SpringBootTest
@@ -29,45 +30,134 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AnswerControllerTest {
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
-    private Gson gson;
-
+    private ObjectMapper objectMapper;
     @MockBean
     private AnswerService answerService;
+    private AnswerDTO answerDTO;
+    private List<AnswerDTO> answerDTOList;
+
+    @BeforeEach
+    void setUp() {
+        answerDTO = new AnswerDTO();
+        answerDTO.setAnswerId(1L);
+        answerDTO.setContent("Test answer");
+        answerDTO.setLike(0);
+        answerDTO.setSelectionStatus(false);
+        answerDTO.setMemberId(1L);
+        answerDTO.setQuestionId(1L);
+
+        answerDTOList = new ArrayList<>();
+        answerDTOList.add(answerDTO);
+    }
 
     @DisplayName("Create Answer")
     @Test
-    void createAnswer() throws Exception {
-        // given
-        AnswerDTO expectedAnswer = new AnswerDTO();
-        expectedAnswer.setAnswerId(1L);
-        expectedAnswer.setContent("test");
-        expectedAnswer.setLike(0);
-        expectedAnswer.setSelectionStatus(false);
-        expectedAnswer.setMemberId(1L);
-        expectedAnswer.setQuestionId(1L);;
-        given(answerService.createAnswer(any(AnswerDTO.POST.class)))
-                .willReturn(expectedAnswer);
-        AnswerDTO.POST postAnswer = new AnswerDTO.POST("text", 1L, 1L);
-        String content = gson.toJson(postAnswer);
+    void testCreateAnswer() throws Exception {
+        AnswerDTO.POST postDTO = new AnswerDTO.POST("Test answer", 1L, 1L);
+        AnswerDTO answerDTO = new AnswerDTO();
+        answerDTO.setContent(postDTO.getContent());
+        answerDTO.setMemberId(postDTO.getMemberId());
+        answerDTO.setQuestionId(postDTO.getQuestionId());
 
-        // when
-        ResultActions actions = mockMvc.perform(post("/api/answers")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content)
-        );
+        when(answerService.createAnswer(any(AnswerDTO.POST.class))).thenReturn(answerDTO);
 
-        // then
-        actions.andExpect(status().isCreated())
-                .andExpect(result -> {
-                    String resultContent = result.getResponse().getContentAsString();
-                    AnswerDTO resultAnswer = gson.fromJson(resultContent, AnswerDTO.class);
+        mockMvc.perform(post("/api/answers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.content").value("Test answer"));
 
-                    if (!expectedAnswer.equals(resultAnswer)) {
-                        throw new AssertionError("Unexpected response result: " + resultContent);
-                    }
-                });
+        verify(answerService, times(1)).createAnswer(any(AnswerDTO.POST.class));
+    }
+
+    @DisplayName("Update Answer")
+    @Test
+    void testUpdateAnswer() throws Exception {
+        Long id = 1L;
+        when(answerService.updateAnswer(id, answerDTO)).thenReturn(answerDTO);
+
+        mockMvc.perform(put("/api/answers/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(answerDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Test answer"));
+
+        verify(answerService, times(1)).updateAnswer(id, answerDTO);
+    }
+
+    @DisplayName("Delete Answer")
+    @Test
+    void testDeleteAnswer() throws Exception {
+        Long id = 1L;
+        doNothing().when(answerService).deleteAnswer(id);
+
+        mockMvc.perform(delete("/api/answers/{id}", id))
+                .andExpect(status().isNoContent());
+
+        verify(answerService, times(1)).deleteAnswer(id);
+    }
+
+    @DisplayName("get AnswerList")
+    @Test
+    void testFindAllAnswers() throws Exception {
+        when(answerService.findAllAnswers()).thenReturn(answerDTOList);
+
+        mockMvc.perform(get("/api/answers"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].content").value("Test answer"));
+
+        verify(answerService, times(1)).findAllAnswers();
+    }
+
+    @DisplayName("get Answer By AnswerId")
+    @Test
+    void testFindAnswerById() throws Exception {
+        Long id = 1L;
+        when(answerService.findAnswer(id)).thenReturn(answerDTO);
+
+        mockMvc.perform(get("/api/answers/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Test answer"));
+        verify(answerService, times(1)).findAnswer(id);
+    }
+
+    @DisplayName("get AnswerList By MemberId")
+    @Test
+    void testFindAnswersByMemberId() throws Exception {
+        Long memberId = 1L;
+        when(answerService.findAnswersByMember(memberId)).thenReturn(answerDTOList);
+
+        mockMvc.perform(get("/api/answers/member/{memberId}", memberId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].content").value("Test answer"));
+
+        verify(answerService, times(1)).findAnswersByMember(memberId);
+    }
+
+    @DisplayName("get AnswerList By QuestionId")
+    @Test
+    void testFindAnswersByQuestionId() throws Exception {
+        Long questionId = 1L;
+        when(answerService.findAnswersByQuestion(questionId)).thenReturn(answerDTOList);
+
+        mockMvc.perform(get("/api/answers/question/{questionId}", questionId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].content").value("Test answer"));
+
+        verify(answerService, times(1)).findAnswersByQuestion(questionId);
+    }
+
+    @DisplayName("Accept to Answer Status")
+    @Test
+    void testAcceptAnswer() throws Exception {
+        Long questionId = 1L;
+        Long answerId = 1L;
+        doNothing().when(answerService).acceptAnswer(questionId, answerId);
+
+        mockMvc.perform(put("/api/answers/accept/{questionId}/{answerId}", questionId, answerId))
+                .andExpect(status().isOk());
+
+        verify(answerService, times(1)).acceptAnswer(questionId, answerId);
     }
 }
