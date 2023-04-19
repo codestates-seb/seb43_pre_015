bda3518621edab26f0f7b1d2f90e144c39371286
Hyojin Ju <hyojin.ju@gmx.net>
2023-04-19 14:23:10 +0900

    feat: Structure App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
 import './App.css';
-import styled from 'styled-components';
 import Navbar from './components/Navbar.js';
 import LoginPage from './components/LoginPage.js';
 
 function App() {
   return (
-    <div>
-      <Navbar></Navbar>
-      <LoginPage></LoginPage>
-    </div>
-
+    <>
+      <main>
+        <Navbar />
+        <section className='features'>
+          <LoginPage />
+        </section>
+      </main>
+    </>
   );
 }
-
-const AppBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
-`
 
 export default App;
